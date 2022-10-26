fc815d1ed503cdfc5e7770ecbf9c5ae088fe1020
SwathiThandarpally <114502982+SwathiThandarpally@users.noreply.github.com>
2022-10-26 23:40:53 +0530

    Added program.cs to the repo

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using EmployeewageAssignmentproblem;
+
+namespace EmployeeWage
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            //Console.WriteLine("Welcome to EmployeeWage Computation Program");    WELCOME MESSAGE
+
+            //EmployeeCheck employeeCheck = new EmployeeCheck();
+            //employeeCheck.Display();                                             USECASE-1
+
+            //DailyWage dailyWage = new DailyWage();
+            //dailyWage.Dispalay();                                                USECASE-2
+
+            //AddPartTime addPartTime = new AddPartTime();
+            //addPartTime.Display();                                               USECASE-3
+
+            //SwitchCase switchCase = new SwitchCase();
+            //switchCase.Display();                                                USECASE-4
+
+            //WorkingDays workingDays = new WorkingDays();
+            //workingDays.Display();                                               USECASE-5
+
+            //ConditionHours conditionHours = new ConditionHours();
+            //conditionHours.Display();                                            USECASE-6
+
+            RefactorCode refactorCode = new RefactorCode();
+            refactorCode.ComputeEmpWage();                                       USECASE-7
+
+
+        }
+    }
+}
